d06d216bbac4941dc9dd5786b690489d3e7faf75
Nanahawaw <folarinyetunde@gmail.com>
2023-10-03 20:29:32 +0100

    fixed the toggling

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,8 +1,17 @@
 let btns = document.querySelectorAll(".question-btn");
+let questions = document.querySelectorAll(".question");
 
-function targetButton(event) {
-  let question = e.currentTarget.parentElement.parentElement;
-  question.classList.toggle("show-text");
+function targetButton(e) {
+  let clickedQuestion = e.currentTarget.parentElement.parentElement;
+  questions.forEach(function (question) {
+    if (question !== clickedQuestion) {
+      // Don't collapse the clicked question here
+      question.classList.remove("show-text");
+    }
+  });
+
+  // Toggle the clicked question
+  clickedQuestion.classList.toggle("show-text");
 }
 
 btns.forEach(function (btn) {
